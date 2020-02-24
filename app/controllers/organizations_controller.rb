231051369bb21c28d9cc3ca99ac9252ca3c4ce40
Caheri Aguilar <caheri.aguilar@gmail.com>
2020-02-23 22:40:53 -0600

    add organizations table

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,10 @@
 class OrganizationsController < ApplicationController
   def show
     puts "It's sunny outside!"
+    @organizations = Organization.all
+    puts @organizations
+  end
+  def create
+    puts "first org"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Organization.create([{name: 'KCW', address: '607 W Wrightwood', description: 'keep Caheri warm'}])
